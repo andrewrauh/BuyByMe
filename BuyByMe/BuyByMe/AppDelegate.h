d8bcb9653c9159ff871b72ce62755f02b83bb5ff
Andrew Rauh <andrew@andrewrauh.com>
2013-01-18 21:36:46 -0500

    Initial Commit

---
+++ BuyByMe/BuyByMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BuyByMe
+//
+//  Created by Andrew Rauh on 1/18/13.
+//  Copyright (c) 2013 Andrew Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BuyByMe/BuyByMe/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BuyByMe
+//
+//  Created by Andrew Rauh on 1/18/13.
+//  Copyright (c) 2013 Andrew Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
