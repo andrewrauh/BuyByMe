8f702a4c135b83ed8541a3d29afa899b8410c856
Andrew Rauh <andrew@andrewrauh.com>
2013-01-19 11:53:36 -0500

    added json encoding

---
+++ BuyByMe/BuyByMe/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  BuyByMe
+//
+//  Created by Andrew Rauh on 1/19/13.
+//  Copyright (c) 2013 Andrew Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
