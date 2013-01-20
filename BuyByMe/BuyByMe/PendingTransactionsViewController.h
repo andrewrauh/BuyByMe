47944fd8e1b4df364e58c283f8b25bd4dc02ee6d
Andrew Rauh <andrew@andrewrauh.com>
2013-01-19 19:25:46 -0500

    shit

---
+++ BuyByMe/BuyByMe/PendingTransactionsViewController.h
@@ -0,0 +1,13 @@
+//
+//  PendingTransactionsViewController.h
+//  BuyByMe
+//
+//  Created by Andrew Rauh on 1/19/13.
+//  Copyright (c) 2013 Andrew Rauh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PendingTransactionsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, retain) IBOutlet UITableView *mytableView;
+@end
